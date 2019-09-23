de90faef124e31d7d47c7006b25cabd60bd3b84a
Patrick Whelan <patrickawhelan@gmail.com>
2019-09-23 14:36:29 -0400

    init commit

---
+++ src/components/ReviewForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from React
+
+const ReviewForm = props => {
+  return(
+    <div>
+      <form>
+        <input type =""> </input>
+        <label htmlFor= ""> </label>
+
+        <input type =""> </input>
+        <label htmlFor= ""> </label>
+
+        <select name="">
+          <option value="">Star Rating</option>
+          <option value=""> </option>
+        </select>
+
+        <textarea name=""> </textarea>
+        <label htmlFor=""> </label>
+
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default ReviewForm
