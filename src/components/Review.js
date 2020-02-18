cfcb06d05588d996055806d1847ec409755da8ad
Daniel <hanakoscar@outlook.com>
2020-02-18 09:44:08 -0500

    test init

--- src/components/Review.js
+++ src/components/Review.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Review = (props) => {
 
-
     let nRating
     if (props.reviewData.rating > 9000) {
       nRating = <iframe src="https://giphy.com/embed/tPKoWQJk3cEbC" width="480" height="359" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
@@ -17,7 +16,7 @@ const Review = (props) => {
       <div className="small-11 columns">{props.reviewData.name}</div>
 
       <div className="small-1 columns"><strong>Score</strong></div>
-      <div className="small-11 columns">{nRating} stars</div>
+      <div className="small-11 columns">{nRating} stars!</div>
 
       <div className="small-1 columns"><strong>Review</strong></div>
       <div className="small-11 columns">{props.reviewData.content}</div>
